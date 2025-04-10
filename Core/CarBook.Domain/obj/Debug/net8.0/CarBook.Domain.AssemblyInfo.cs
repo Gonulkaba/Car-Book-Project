1e7f5c2e087debc711780674a6a16223096b3a0a
Gönül Kaba <gonul.kaba123@gmail.com>
2025-04-11 00:00:23 +0300

    Frontend

--- Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
+++ Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72fe900b8a2d8e1c8707b67b8fc0ae293b14d520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b31ae8eeb8394d1e9a0dc402fca23c727aedb20")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
