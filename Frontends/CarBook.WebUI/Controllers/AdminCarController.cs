7d8a5551e8d12a59e63a0a54c51c08b582acd75b
Gönül Kaba <gonul.kaba123@gmail.com>
2025-04-21 00:33:23 +0300

    feat: completed CreateCar page for adding new vehicles

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,12 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
+using CarBook.Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
-    }
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.name,
+                                                    Value = x.brandId.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
+            return View();
+        }
+		[HttpPost]
+		public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createCarDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("http://localhost:5000/api/Cars", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+	}
 }
