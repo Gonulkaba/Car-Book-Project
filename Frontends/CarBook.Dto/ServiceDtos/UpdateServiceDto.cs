ca2163a653f885e75cbc422184d9e2a36b32f7c0
Gönül Kaba <gonul.kaba123@gmail.com>
2025-04-27 23:38:10 +0300

    feat: implemented CRUD functionalities for  FooterAddress, Location, and Service on frontend

--- Frontends/CarBook.Dto/ServiceDtos/UpdateServiceDto.cs
+++ Frontends/CarBook.Dto/ServiceDtos/UpdateServiceDto.cs
@@ -8,7 +8,7 @@ namespace CarBook.Dto.ServiceDtos
 {
 	public class UpdateServiceDto
 	{
-		public int ServiceId { get; set; }
+		public int ServiceID { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public string IconUrl { get; set; }
