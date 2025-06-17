26ff1a9c63f78506fad3b08bb3780fcb3f601719
Gönül Kaba <gonul.kaba123@gmail.com>
2025-06-17 23:58:39 +0300

    feat: SignalR

---
+++ Presentation/CarBook.WebApi/Hubs/CarHub.cs
@@ -0,0 +1,24 @@
+﻿using CarBook.Dto.StatisticDtos;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace CarBook.WebApi.Hubs
+{
+	public class CarHub : Hub
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CarHub(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task SendCarCount()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:5000/api/Statistics/GetCarCount");
+			var values = await responseMessage.Content.ReadAsStringAsync();
+			await Clients.All.SendAsync("ReceiveCarCount",values);
+		}
+	}
+}
--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -35,8 +35,23 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using CarBook.Application.Tools;
 using Microsoft.OpenApi.Models;
+using CarBook.WebApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
 {
@@ -157,6 +172,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
@@ -164,5 +181,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<CarHub>("/carhub");
 
 app.Run();
